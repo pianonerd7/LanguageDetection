a96037d01cae809573336d42c4534e4d73a53cf6
Anna He <jxh604@case.edu>
2017-02-02 11:40:05 +0800

    refactored get language, wrote skeleton for processing test LM

--- utility.py
+++ utility.py
@@ -3,16 +3,14 @@ language_model = dict()
 language_count = dict()
 SMOOTHING_CONST = 1
 
-def read_from_file(in_file):
+def file_to_probability(in_file):
     for line in file(in_file):
         process_training_line(line)
     convert_count_to_probability()
 
 # <4gram, dict<language, probability>>
 def process_training_line(line):
-    space_index = line.index(' ')
-    language = line[:space_index]
-    data = line[space_index + 1:]
+    language, data = get_language_and_data(line)
 
     if language not in language_count:
         language_count[language] = 0
@@ -29,6 +27,7 @@ def process_training_line(line):
         else:
             cur_4_gram_dict[language] += 1
             language_count[language] += 1
+    return language_model
 
 def convert_count_to_probability():
     for cur_4_gram in language_model:
@@ -36,5 +35,23 @@ def convert_count_to_probability():
         for language in cur_4_gram_dict:
             cur_4_gram_dict[language] = cur_4_gram_dict[language]/float(language_count[language])
 
-read_from_file("./TxtFiles/input.train.txt")
-print language_model
+def file_to_language_suggestion(in_file, out_file, LM)
+    output_file = file('output-file', 'w')
+    for line in file(in_file):
+        prediction = predict_language(line)
+        output_file.write(prediction)
+
+def predict_language(line):
+    language, data = get_language_and_data(line)
+    #compute here
+    new_language = null
+    return new_language + " " + data #<-- should use stringbuilder instead? 
+
+#returns the language of the string, and the string to extract n-gram from
+def get_language_and_data(line):
+    space_index = line.index(' ')
+    language = line[:space_index]
+    data = line[space_index + 1:]
+    return language, data
+
+def get_grams(line):
