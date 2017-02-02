68eb0db95af89645c4f369e5d8202da65f3ccaac
Anna He <jxh604@case.edu>
2017-02-02 11:56:03 +0800

    refactored to get all grams, and generified process_training_line

--- utility.py
+++ utility.py
@@ -2,6 +2,7 @@
 language_model = dict()
 language_count = dict()
 SMOOTHING_CONST = 1
+GRAM_SIZE = 4
 
 def file_to_probability(in_file):
     for line in file(in_file):
@@ -15,20 +16,21 @@ def process_training_line(line):
     if language not in language_count:
         language_count[language] = 0
 
-    for index, char in enumerate(data):
-        cur_4_gram = data[index:index + 4]
-
-        if cur_4_gram not in language_model:
-            language_model[cur_4_gram] = dict()
-        cur_4_gram_dict = language_model[cur_4_gram]
-        if language not in cur_4_gram_dict:
-            cur_4_gram_dict[language] = SMOOTHING_CONST + 1
+    all_grams = get_grams(data)
+    
+    for gram in all_grams:
+        if gram not in language_model:
+            language_model[gram] = dict()
+        cur_gram_dict = language_model[gram]
+        if language not in cur_gram_dict:
+            cur_gram_dict[language] = SMOOTHING_CONST + 1
             language_count[language] += SMOOTHING_CONST + 1
         else:
-            cur_4_gram_dict[language] += 1
+            cur_gram_dict[language] += 1
             language_count[language] += 1
-    return language_model
 
+    return language_model, language_count
+        
 def convert_count_to_probability():
     for cur_4_gram in language_model:
         cur_4_gram_dict = language_model[cur_4_gram]
@@ -47,11 +49,17 @@ def predict_language(line):
     new_language = null
     return new_language + " " + data #<-- should use stringbuilder instead? 
 
-#returns the language of the string, and the string to extract n-gram from
+# Returns the language of the string, and the string to extract n-gram from
 def get_language_and_data(line):
     space_index = line.index(' ')
     language = line[:space_index]
     data = line[space_index + 1:]
     return language, data
 
-def get_grams(line):
+# Given a string, returns all the n-gram in that string where the length of the gram 
+# is specified by the GRAM_SIZE constant
+def get_grams(data):
+    grams = []
+    for index, char in enumerate(data):
+        grams.append(data[index:index + GRAM_SIZE])
+    return grams
